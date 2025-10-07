d2338469b0a9deb50c709ffae985c9684e8a5864
Ayleen <ayleen.k.kyrstin@gmail.com>
2025-10-07 18:30:57 +0200

    Add preorder interest form and goodreads link

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("signupForm");
 const message = document.getElementById("formMessage");
 
 form.addEventListener("submit", function(e) {
-  e.preventDefault(); // stop normal redirect
+  e.preventDefault();
 
   const formData = new FormData(form);
 
@@ -19,3 +19,25 @@ form.addEventListener("submit", function(e) {
     alert("Something went wrong. Please try again.");
   });
 });
+
+const preorderForm = document.getElementById("preorderForm");
+const preorderMessage = document.getElementById("formMessagePreorder");
+
+preorderForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const formData2 = new FormData(preorderForm);
+  
+  fetch("https://docs.google.com/forms/d/e/1FAIpQLSepenLS9plJ2DDbICjHcrPP7-MuS0z4wSsl8x-Wvp3L-TNMsw/formResponse", {
+    method: "POST",
+    body: formData2,
+    mode: "no-cors"
+  })
+  .then(() => {
+    preorderForm.style.display = "none"; 
+    preorderMessage.style.display = "block";
+  })
+  .catch(() => {
+    alert("Something went wrong. Please try again.");
+  });
+});
+
